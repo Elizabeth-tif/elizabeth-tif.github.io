4829a3fd4dff82ba7db895dbec0a3068ef6ad5a0
Elizabeth Gita <elizabeth.gita.tif423@polban.ac.id>
2024-04-12 17:19:46 +0700

    Final commit news page

--- js/main.js
+++ js/main.js
@@ -5,29 +5,40 @@ mntoggle.addEventListener('click', function () {
     nav.classList.toggle('menushow');
 })
 
-function insertDataScrap() {
-    fetch('headlines.json')
-        .then(response => response.json())
-        .then(data => {
-            var table = $('#news-table').DataTable();
-            data.forEach((obj, index) => {
-                table.row.add([
-                    index + 1,
-                    obj.headline,
-                    obj.category,
-                    obj.publish_time,
-                    obj.storing_time
-                ]);
+$(document).ready(function () {
+    const mntoggle = $('.menu-toggle input');
+    const nav = $('nav ul');
+
+    mntoggle.on('click', function () {
+        nav.toggleClass('menushow');
+    });
+
+    function insertDataScrap() {
+        $.getJSON('headlines.json')
+            .done(function (data) {
+                var table = $('#news-table').DataTable({
+                    "columnDefs": [
+                        {"className": "dt-center", "targets": "_all"}
+                    ],
+                    "drawCallback": function () {
+                        $('#news-table td').css('border', '1px solid #b784b7', 'z-index', '1');
+                    }
+                });
+
+                data.forEach(function (obj, index) {
+                    table.row.add([
+                        index + 1,
+                        obj.headline,
+                        obj.category,
+                        obj.waktu_publish,
+                        obj.storing_time
+                    ]).draw(false);
+                });
+            })
+            .fail(function (jqXHR, textStatus, errorThrown) {
+                console.error('Error:', textStatus, errorThrown);
             });
-            table.draw();
-        })
-        .catch(error => {
-            console.error('Error:', error);
-        });
-}
+    }
 
-document.addEventListener('DOMContentLoaded', insertDataScrap);
-
-$('#news-table').on('draw.dt', function () {
-    $('#news-table td').css('text-align', 'center', 'border', '1px solid #b784b7','z-index', '1');
+    insertDataScrap();
 });
