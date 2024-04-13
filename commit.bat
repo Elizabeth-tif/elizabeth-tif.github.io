3c9089323ed9afc24662b1150e2615348d73fd94
Elizabeth Gita <elizabeth.gita.tif423@polban.ac.id>
2024-04-13 11:14:53 +0700

    Auto update ~1

---
+++ commit.bat
@@ -0,0 +1,15 @@
+@echo off
+setlocal EnableDelayedExpansion
+
+cd "C://Users//Asus//OneDrive/" "-/" "Politeknik/" "Negeri/" "Bandung//Materi/" "Kuliah//Semester/" "2//MK/" "Proyek/\Elizabeth-tif.github.io"
+git "add" "--all"
+SET _INTERPOLATION_0=
+FOR /f "delims=" %%a in ('timestamp') DO (SET "_INTERPOLATION_0=!_INTERPOLATION_0! %%a")
+git "commit" "-am" "Auto update !_INTERPOLATION_0:~1!"
+git "push" "origin" "main"
+
+EXIT /B %ERRORLEVEL%
+
+:timestamp
+date "+at %H:%M:%S on %d/%m/%Y"
+EXIT /B 0
