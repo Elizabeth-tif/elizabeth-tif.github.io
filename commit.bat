78249e70cea4ec56108f7d35f1f407680c0e137e
Elizabeth Gita <elizabeth.gita.tif423@polban.ac.id>
2024-04-13 11:30:35 +0700

    Auto update

--- commit.bat
+++ commit.bat
@@ -3,12 +3,7 @@ setlocal EnableDelayedExpansion
 
 cd "C:\Users\Asus\OneDrive - Politeknik Negeri Bandung\Materi Kuliah\Semester 2\MK Proyek\Elizabeth-tif.github.io"
 git add --all
-
-REM Get current timestamp
-for /f "tokens=2" %%a in ('date /t') do set "_DATE=%%a"
-for /f "tokens=1-3 delims=:" %%b in ('time /t') do set "_TIME=%%b:%%c"
-
-git commit -am "Auto update at !_TIME! on !_DATE!"
+git commit -am "Auto update"
 git push origin main
 
 exit /b %ERRORLEVEL%
