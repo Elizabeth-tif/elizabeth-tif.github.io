9233d15835d4dc5ee342af53c4172af9c3da7656
Elizabeth Gita <elizabeth.gita.tif423@polban.ac.id>
2024-04-14 06:11:48 +0700

    Auto update

--- commit.bat
+++ commit.bat
@@ -2,6 +2,7 @@
 setlocal EnableDelayedExpansion
 
 cd "C:\Users\Asus\OneDrive - Politeknik Negeri Bandung\Materi Kuliah\Semester 2\MK Proyek\Elizabeth-tif.github.io"
+"C:\Users\Asus\OneDrive - Politeknik Negeri Bandung\Materi Kuliah\Semester 2\MK Proyek\Elizabeth-tif.github.io\scrapper.exe"
 git add --all
 git commit -am "Auto update"
 git push origin main
