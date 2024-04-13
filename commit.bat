863fe9a5de6d9ff700afc10e430f544c8c004be6
Elizabeth Gita <elizabeth.gita.tif423@polban.ac.id>
2024-04-13 11:17:50 +0700

    Auto update at 11:17 on

--- commit.bat
+++ commit.bat
@@ -1,15 +1,14 @@
 @echo off
 setlocal EnableDelayedExpansion
 
-cd "C:\Users\Asus\OneDrive - Politeknik Negeri Bandung\Materi Kuliah\Semester 2\MK Proyek\Elizabeth-tif.github.io"
-git "add" "--all"
-SET _INTERPOLATION_0=
-FOR /f "delims=" %%a in ('timestamp') DO (SET "_INTERPOLATION_0=!_INTERPOLATION_0! %%a")
-git "commit" "-am" "Auto update !_INTERPOLATION_0:~1!"
-git "push" "origin" "main"
+cd "C:\Users\Asus\OneDrive\-Politeknik\Negeri\Bandung\Materi\Kuliah\Semester\2\MK\Proyek\Elizabeth-tif.github.io"
+git add --all
 
-EXIT /B %ERRORLEVEL%
+REM Get current timestamp
+for /f "tokens=2" %%a in ('date /t') do set "_DATE=%%a"
+for /f "tokens=1-3 delims=:" %%b in ('time /t') do set "_TIME=%%b:%%c"
 
-:timestamp
-date "+at %H:%M:%S on %d/%m/%Y"
-EXIT /B 0
+git commit -am "Auto update at !_TIME! on !_DATE!"
+git push origin main
+
+exit /b %ERRORLEVEL%
